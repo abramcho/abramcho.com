503b48d3252a0e16bf5d41685b4f4e69868e4c11
Abraham <48066061+abramcho@users.noreply.github.com>
2024-11-05 15:17:40 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,30 @@
 <h1>Abraham's web page</h1>
 
 <ul>
-	<li>
-		<a href="https://www.linkedin.com/in/abrahamarteagaherrera/">linkedin</a>
-	</li>
+	
 	<li>
 		<a href="https://www.instagram.com/abramcho_/">Instagram</a>
 	</li>
 	<li>
+		<a href="https://www.youtube.com/@abramcho">YouTube</a>
+	</li>
+	<li>
+		<a href="https://www.tiktok.com/@abramcho__">TikTok 1</a>
+	</li>
+	<li>
+		<a href="https://www.tiktok.com/@abramcho_">TikTok 2</a>
+	</li>
+	<li>
 		<a href="https://vsco.co/abramcho-/gallery">vsco</a>
+	</li>
+	<li>
+		<a href="https://www.linkedin.com/in/abrahamarteagaherrera/">linkedin</a>
 	</li>
 	<li>
 		<a href="https://github.com/abramcho">Github</a>
 	</li>
 	<li>
 		<a href="https://twitter.com/abramcho_">Twitter</a>
-	</li>
-	<li>
-		<a href="https://www.youtube.com/@dumio8480">Podcast</a>
 	</li>
 </ul>
 
